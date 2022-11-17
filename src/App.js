a94ffe9343090a47efbd3069840482e10f4230b3
Taro <49375207+tarowillmakeitright@users.noreply.github.com>
2022-11-17 12:51:49 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import TwitterTimeline from './components/TwitterTimeline'
+
+function App() {
+  return (
+    <TwitterTimeline />
+  );
+}
+
+export default App;
---
+++ src/components/TwitterTimeline.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { TwitterTimelineEmbed } from "react-twitter-embed";
+import '../components/styles/style.css';
+
+function TwitterTimeline () {
+  return (
+    <div className="twitterTimeline">
+      <TwitterTimelineEmbed
+        sourceType="profile"
+        screenName="gigazine"
+        options={{ height: 400 }}
+      />
+      <TwitterTimelineEmbed
+        sourceType="profile"
+        screenName="nikkei"
+        options={{ height: 400 }}
+      />
+      <TwitterTimelineEmbed
+        sourceType="profile"
+        screenName="kyodo_official"
+        options={{ height: 400 }}
+      />
+      <TwitterTimelineEmbed
+        sourceType="profile"
+        screenName="sak_07_"
+        options={{ height: 400 }}
+      />
+      <TwitterTimelineEmbed
+        sourceType="profile"
+        screenName="YahooNewsTopics"
+        options={{ height: 400 }}
+      />
+      <TwitterTimelineEmbed
+        sourceType="profile"
+        screenName="NetflixJP"
+        options={{ height: 400 }}
+      />
+      <TwitterTimelineEmbed
+        sourceType="profile"
+        screenName="zoknd"
+        options={{ height: 400 }}
+      />
+      <TwitterTimelineEmbed
+        sourceType="profile"
+        screenName="TechCrunch"
+        options={{ height: 400 }}
+      />
+    </div>
+  );
+}
+
+export default TwitterTimeline;
