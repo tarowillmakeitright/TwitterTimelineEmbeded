06960dc4721aa4ed877ee4e222582a62acc316e1
Taro <49375207+tarowillmakeitright@users.noreply.github.com>
2022-12-25 17:54:57 +0900

    Add files via upload

--- src/components/CustomizedTables.js
+++ src/components/CustomizedTables.js
@@ -44,7 +44,7 @@ const rows = [
     "イーロンマスク",
     // ジャンル
     "イノベーション",
-    <a href="https://www.amazon.co.jp/dp/B00NQ3QONK" target="_blank" rel="noreferrer">
+    <a href="www.amazon.co.jp/dp/B00NQ3QONK" target="_blank" rel="noreferrer">
       Amazon
     </a>
   ),
@@ -705,6 +705,1043 @@ const rows = [
     "生き方",
     <a
       href="https://www.amazon.co.jp/dp/4560071837"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "ザ・ゴール",
+    // 著者
+    "エリヤフ ゴールドラット",
+    // おすすめしている人
+    "ジェフベゾス",
+    // ジャンル
+    "経営",
+    <a
+      href="https://www.amazon.co.jp/dp/B0081M7YEE"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "イノベーションへの解 ",
+    // 著者
+    "クレイトン・M・クリステンセン",
+    // おすすめしている人
+    "ジェフベゾス",
+    // ジャンル
+    "経営",
+    <a
+      href="https://www.amazon.co.jp/dp/4798104930"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "ハーバード・ビジネススクール“クリステンセン”教授の 「イノベーションのジレンマ」入門",
+    // 著者
+    "グローバルタスクフォース",
+    // おすすめしている人
+    "ジェフベゾス",
+    // ジャンル
+    "経営",
+    <a
+      href="https://www.amazon.co.jp/dp/B00T7FIFC4"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "ビジョナリー・カンパニーZERO ゼロから事業を生み出し、偉大で永続的な企業になる",
+    // 著者
+    "ジム・コリンズ",
+    // おすすめしている人
+    "ジェフベゾス",
+    // ジャンル
+    "経営",
+    <a
+      href="https://www.amazon.co.jp/dp/B09B966P33"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "日の名残り",
+    // 著者
+    "カズオ・イシグロ",
+    // おすすめしている人
+    "ジェフベゾス",
+    // ジャンル
+    "小説",
+    <a
+      href="https://www.amazon.co.jp/dp/B009DEMAKM"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "盲目の時計職人",
+    // 著者
+    "リチャード・ドーキンス",
+    // おすすめしている人
+    "ジェフベゾス",
+    // ジャンル
+    "進化論",
+    <a
+      href="https://www.amazon.co.jp/dp/4152085576"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "データ・ドリブン・マーケティング―――最低限知っておくべき15の指標",
+    // 著者
+    "マーク・ジェフリー",
+    // おすすめしている人
+    "ジェフベゾス",
+    // ジャンル
+    "データ解析",
+    <a
+      href="https://www.amazon.co.jp/dp/4478039631"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "人月の神話",
+    // 著者
+    "フレデリック・P・ブルックス，Jr. ",
+    // おすすめしている人
+    "ジェフベゾス",
+    // ジャンル
+    "リスクマネジメント",
+    <a
+      href="https://www.amazon.co.jp/dp/B0998ZTVTD"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "ダイヤモンド・エイジ　上下",
+    // 著者
+    "ニール スティーヴンスン",
+    // おすすめしている人
+    "ジェフベゾス",
+    // ジャンル
+    "小説",
+    <a
+      href="https://www.amazon.co.jp/dp/B09Q8CFNZG"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "ブラック・スワン[上][下]―不確実性とリスクの本質",
+    // 著者
+    "ナシーム・ニコラス・タレブ",
+    // おすすめしている人
+    "ジェフベゾス",
+    // ジャンル
+    "リスクマネジメント",
+    <a
+      href="https://www.amazon.co.jp/dp/4478001251"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "小さなチーム、大きな仕事　働き方の新しいスタンダード",
+    // 著者
+    "ジェイソン フリード ",
+    // おすすめしている人
+    "ジェフベゾス",
+    // ジャンル
+    "経営",
+    <a
+      href="https://www.amazon.co.jp/dp/B01NCJVL06"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "リーン・シンキング",
+    // 著者
+    "ジェームズ・P・ウォーマック",
+    // おすすめしている人
+    "ジェフベゾス",
+    // ジャンル
+    "経営",
+    <a
+      href="https://www.amazon.co.jp/dp/4822246728"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "私のウォルマート商法 すべて小さく考えよ",
+    // 著者
+    "サム・ウォルトン",
+    // おすすめしている人
+    "ジェフベゾス",
+    // ジャンル
+    "リーダー論",
+    <a
+      href="https://www.amazon.co.jp/dp/406256677X"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "ドラッカー名著集1 経営者の条件",
+    // 著者
+    "Ｐ Ｆ ドラッカー",
+    // おすすめしている人
+    "ジェフベゾス",
+    // ジャンル
+    "リーダー論",
+    <a
+      href="https://www.amazon.co.jp/dp/B0081M7Z20"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "楽観主義者の未来予測(上)(下): テクノロジーの爆発的進化が世界を豊かにする",
+    // 著者
+    "ピーター・H. ディアマンディス",
+    // おすすめしている人
+    "ビルゲイツ",
+    // ジャンル
+    "未来予測",
+    <a
+      href="https://www.amazon.co.jp/dp/4152094362"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "ヤバい経済学　悪ガキ教授が世の裏側を探検する",
+    // 著者
+    "スティーヴン・Ｊ・ダブナー",
+    // おすすめしている人
+    "ビルゲイツ",
+    // ジャンル
+    "経済学",
+    <a
+      href="https://www.amazon.co.jp/dp/B00LF3TU98"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "超ヤバい経済学",
+    // 著者
+    "スティーヴン・Ｄ・レヴィット",
+    // おすすめしている人
+    "ビルゲイツ",
+    // ジャンル
+    "経済学",
+    <a
+      href="https://www.amazon.co.jp/dp/B00LF3TUBG"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "大脱出――健康、お金、格差の起原",
+    // 著者
+    "アンガス・ディートン",
+    // おすすめしている人
+    "ビルゲイツ",
+    // ジャンル
+    "経済学",
+    <a
+      href="https://www.amazon.co.jp/dp/4622078708"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "21世紀の資本",
+    // 著者
+    "トマ・ピケティ",
+    // おすすめしている人
+    "ビルゲイツ",
+    // ジャンル
+    "経済学",
+    <a
+      href="https://www.amazon.co.jp/dp/B00VQ75FAQ"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "貧乏人の経済学――もういちど貧困問題を根っこから考える",
+    // 著者
+    "アビジット・V・バナジー ",
+    // おすすめしている人
+    "ビルゲイツ",
+    // ジャンル
+    "経済学",
+    <a
+      href="https://www.amazon.co.jp/dp/4622076519"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "絶望を希望に変える経済学 社会の重大問題をどう解決するか",
+    // 著者
+    "アビジット・Ｖ・バナジー",
+    // おすすめしている人
+    "ビルゲイツ",
+    // ジャンル
+    "経済学",
+    <a
+      href="https://www.amazon.co.jp/dp/B0876Z87XK"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "Measure What Matters　伝説のベンチャー投資家がGoogleに教えた成功手法 OKR",
+    // 著者
+    "ジョン・ドーア",
+    // おすすめしている人
+    "ビルゲイツ",
+    // ジャンル
+    "経営",
+    <a
+      href="https://www.amazon.co.jp/dp/B07JCZVFZ9"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "コンテナ物語　世界を変えたのは「箱」の発明だった",
+    // 著者
+    "マルク・レビンソン",
+    // おすすめしている人
+    "ビルゲイツ",
+    // ジャンル
+    "経営",
+    <a
+      href="https://www.amazon.co.jp/dp/B07Z4DNFG7"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "ディズニーＣＥＯが実践する１０の原則",
+    // 著者
+    "ロバート アイガー",
+    // おすすめしている人
+    "ビルゲイツ",
+    // ジャンル
+    "経営",
+    <a
+      href="https://www.amazon.co.jp/dp/B086LBLXJX"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "ＳＨＯＥ　ＤＯＧ　靴にすべてを。",
+    // 著者
+    "フィル・ナイト",
+    // おすすめしている人
+    "ビルゲイツ",
+    // ジャンル
+    "経営",
+    <a
+      href="https://www.amazon.co.jp/dp/B0757FCXC7"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "人と企業はどこで間違えるのか？",
+    // 著者
+    "John Brooks",
+    // おすすめしている人
+    "ビルゲイツ",
+    // ジャンル
+    "経営",
+    <a
+      href="https://www.amazon.co.jp/dp/B00RKTGXO6"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "FACTFULNESS　10の思い込みを乗り越え、データを基に世界を正しく見る習慣",
+    // 著者
+    "ハンス・ロスリング",
+    // おすすめしている人
+    "ビルゲイツ",
+    // ジャンル
+    "経営",
+    <a
+      href="https://www.amazon.co.jp/dp/B07LG7TG5N"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "RANGE　知識の「幅」が最強の武器になる",
+    // 著者
+    "デイビッド・エプスタイン",
+    // おすすめしている人
+    "ビルゲイツ",
+    // ジャンル
+    "経営",
+    <a
+      href="https://www.amazon.co.jp/dp/B0868DR365"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "ドーキンス博士が教える「世界の秘密」",
+    // 著者
+    "リチャード・ドーキンス",
+    // おすすめしている人
+    "ビルゲイツ",
+    // ジャンル
+    "科学",
+    <a
+      href="https://www.amazon.co.jp/dp/4152093463"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "データを正しく見るための数学的思考　数学の言葉で世界を見る",
+    // 著者
+    "ジョーダン エレンバーグ",
+    // おすすめしている人
+    "ビルゲイツ",
+    // ジャンル
+    "データ解析",
+    <a
+      href="https://www.amazon.co.jp/dp/B012RSE58U"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "シグナル&ノイズ 天才データアナリストの「予測学」",
+    // 著者
+    "ネイト・シルバー",
+    // おすすめしている人
+    "ビルゲイツ",
+    // ジャンル
+    "予想学",
+    <a
+      href="https://www.amazon.co.jp/dp/4822249808"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "ビッグヒストリー われわれはどこから来て、どこへ行くのか――宇宙開闢から138億年の「人間」史",
+    // 著者
+    "デヴィッド・クリスチャン",
+    // おすすめしている人
+    "ビルゲイツ",
+    // ジャンル
+    "歴史",
+    <a
+      href="https://www.amazon.co.jp/dp/4750344214"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "あなたの人生の意味　先人に学ぶ「惜しまれる生き方」",
+    // 著者
+    "デイヴィッド ブルックス",
+    // おすすめしている人
+    "ビルゲイツ",
+    // ジャンル
+    "生き方",
+    <a
+      href="https://www.amazon.co.jp/dp/B01MZDWOYK"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "睡眠こそ最強の解決策である",
+    // 著者
+    "マシュー・ウォーカー",
+    // おすすめしている人
+    "ビルゲイツ",
+    // ジャンル
+    "睡眠学",
+    <a
+      href="https://www.amazon.co.jp/dp/B07D3N4NQM"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "マインドセット：「やればできる！」の研究",
+    // 著者
+    "キャロル・S・ドゥエック",
+    // おすすめしている人
+    "ビルゲイツ",
+    // ジャンル
+    "心理学",
+    <a
+      href="https://www.amazon.co.jp/dp/B01FTIQY6G"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "ごく平凡な記憶力の私が1年で全米記憶力チャンピオンになれた理由 ",
+    // 著者
+    "ジョシュア・フォア",
+    // おすすめしている人
+    "ビルゲイツ",
+    // ジャンル
+    "記憶術",
+    <a
+      href="https://www.amazon.co.jp/dp/4767811805"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "頭を「からっぽ」にするレッスン 10分間瞑想でマインドフルに生きる",
+    // 著者
+    "アンディ・プディコム",
+    // おすすめしている人
+    "ビルゲイツ",
+    // ジャンル
+    "心理学",
+    <a
+      href="https://www.amazon.co.jp/dp/B08HC9JPH3"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "無人の兵団　AI、ロボット、自律型兵器と未来の戦争",
+    // 著者
+    "ポール シャーレ",
+    // おすすめしている人
+    "ビルゲイツ",
+    // ジャンル
+    "科学",
+    <a
+      href="https://www.amazon.co.jp/dp/B07V3RKR26"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "探求 上下: エネルギーの世紀",
+    // 著者
+    "ダニエル ヤーギン",
+    // おすすめしている人
+    "ビルゲイツ",
+    // ジャンル
+    "科学",
+    <a
+      href="https://www.amazon.co.jp/dp/4532168317"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "ホワット・イフ？――野球のボールを光速で投げたらどうなるか",
+    // 著者
+    "ランドール マンロー",
+    // おすすめしている人
+    "ビルゲイツ",
+    // ジャンル
+    "物理学",
+    <a
+      href="https://www.amazon.co.jp/dp/B010COOF3G"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "これが物理学だ！　マサチューセッツ工科大学「感動」講義",
+    // 著者
+    "ウォルター・ルーウィン",
+    // おすすめしている人
+    "ビルゲイツ",
+    // ジャンル
+    "物理学",
+    <a
+      href="https://www.amazon.co.jp/dp/B00B7UNYD2"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "生命、エネルギー、進化",
+    // 著者
+    "ニック・レーン",
+    // おすすめしている人
+    "ビルゲイツ",
+    // ジャンル
+    "科学",
+    <a
+      href="https://www.amazon.co.jp/dp/B01N0B10U4"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "人類を変えた素晴らしき10の材料: その内なる宇宙を探険する ",
+    // 著者
+    "マーク・ミーオドヴニク",
+    // おすすめしている人
+    "ビルゲイツ",
+    // ジャンル
+    "科学",
+    <a
+      href="https://www.amazon.co.jp/dp/4772695478"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "いま、希望を語ろう　末期がんの若き医師が家族と見つけた「生きる意味」",
+    // 著者
+    "ポール カラニシ",
+    // おすすめしている人
+    "ビルゲイツ",
+    // ジャンル
+    "ノンフィクション",
+    <a
+      href="https://www.amazon.co.jp/dp/B01MXDUXZ3"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "モスクワの伯爵",
+    // 著者
+    "エイモア トールズ",
+    // おすすめしている人
+    "ビルゲイツ",
+    // ジャンル
+    "小説",
+    <a
+      href="https://www.amazon.co.jp/dp/4152098600"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "レオナルド・ダ・ヴィンチ　上下",
+    // 著者
+    "ウォルター・アイザックソン",
+    // おすすめしている人
+    "ビルゲイツ",
+    // ジャンル
+    "歴史の人",
+    <a
+      href="https://www.amazon.co.jp/dp/B09TSZS526"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "ガイトナー回顧録 ―金融危機の真相",
+    // 著者
+    "ティモシー・Ｆ・ガイトナー",
+    // おすすめしている人
+    "ビルゲイツ",
+    // ジャンル
+    "経済学",
+    <a
+      href="https://www.amazon.co.jp/dp/B079GQPFFN"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "バーナンキは正しかったか? FRBの真相",
+    // 著者
+    "デイビッド・ウェッセル",
+    // おすすめしている人
+    "ビルゲイツ",
+    // ジャンル
+    "歴史の人",
+    <a
+      href="https://www.amazon.co.jp/dp/4023308048"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "現代中国の父 トウ小平　上下",
+    // 著者
+    "エズラ・F・ヴォーゲル",
+    // おすすめしている人
+    "ビルゲイツ",
+    // ジャンル
+    "歴史の人",
+    <a
+      href="https://www.amazon.co.jp/dp/4532168856"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "21世紀の啓蒙 上：理性、科学、ヒューマニズム、進歩",
+    // 著者
+    "スティーブン・ピンカー",
+    // おすすめしている人
+    "ビルゲイツ",
+    // ジャンル
+    "社会学",
+    <a
+      href="https://www.amazon.co.jp/dp/B082S7Q2GN"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "かつての超大国アメリカ: どこで間違えたのかどうすれば復活できるのか ",
+    // 著者
+    "トーマス フリードマン",
+    // おすすめしている人
+    "ビルゲイツ",
+    // ジャンル
+    "国家論",
+    <a
+      href="https://www.amazon.co.jp/dp/4532168457"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "国家はなぜ衰退するのか　権力・繁栄・貧困の起源（上）（下）",
+    // 著者
+    "ダロン・アセモグル",
+    // おすすめしている人
+    "ビルゲイツ",
+    // ジャンル
+    "国家論",
+    <a
+      href="https://www.amazon.co.jp/dp/B00EJA1CJA"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "遺伝子‐親密なる人類史‐　上下 ",
+    // 著者
+    "シッダールタ ムカジー",
+    // おすすめしている人
+    "ビルゲイツ",
+    // ジャンル
+    "細胞学",
+    <a
+      href="https://www.amazon.co.jp/dp/B08XKLMBSV"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "繁栄　明日を切り拓くための人類１０万年史",
+    // 著者
+    "マット・リドレー ",
+    // おすすめしている人
+    "ビルゲイツ",
+    // ジャンル
+    "細胞学",
+    <a
+      href="https://www.amazon.co.jp/dp/B00EU9SKKE"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "昨日までの世界(上)(下)―文明の源流と人類の未来",
+    // 著者
+    "ジャレド・ダイアモンド ",
+    // おすすめしている人
+    "ビルゲイツ",
+    // ジャンル
+    "人類史",
+    <a
+      href="https://www.amazon.co.jp/dp/B074T7YFKJ"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "銃・病原菌・鉄　上下巻",
+    // 著者
+    "ジャレド ダイアモンド",
+    // おすすめしている人
+    "ビルゲイツ",
+    // ジャンル
+    "人類史",
+    <a
+      href="https://www.amazon.co.jp/dp/B00DNMG8Q2"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "危機と人類（上）（下）",
+    // 著者
+    "ジャレド ダイアモンド",
+    // おすすめしている人
+    "ビルゲイツ",
+    // ジャンル
+    "人類史",
+    <a
+      href="https://www.amazon.co.jp/dp/B07ZJ8DJNB"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "２１　Ｌｅｓｓｏｎｓ　２１世紀の人類のための２１の思考",
+    // 著者
+    "ユヴァル・ノア・ハラリ ",
+    // おすすめしている人
+    "ビルゲイツ",
+    // ジャンル
+    "人類史",
+    <a
+      href="https://www.amazon.co.jp/dp/B09KN7H8HD"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "ホモ・デウス　上下　テクノロジーとサピエンスの未来 ホモ・デウス　テクノロジーとサピエンスの未来",
+    // 著者
+    "ユヴァル・ノア・ハラリ ",
+    // おすすめしている人
+    "ビルゲイツ",
+    // ジャンル
+    "人類史",
+    <a
+      href="https://www.amazon.co.jp/dp/B0BDZG7VXX"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "サピエンス全史（上）（下）　文明の構造と人類の幸福 サピエンス全史　文明の構造と人類の幸福",
+    // 著者
+    "ユヴァル・ノア・ハラリ ",
+    // おすすめしている人
+    "ビルゲイツ",
+    // ジャンル
+    "人類史",
+    <a
+      href="https://www.amazon.co.jp/dp/B01LW7JZLC"
+      target="_blank"
+      rel="noreferrer"
+    >
+      Amazon
+    </a>
+  ),
+  createData(
+    // 書籍名
+    "オリジン・ストーリー",
+    // 著者
+    "デイヴィッド・クリスチャン",
+    // おすすめしている人
+    "ビルゲイツ",
+    // ジャンル
+    "人類史",
+    <a
+      href="https://www.amazon.co.jp/dp/4480858180"
       target="_blank"
       rel="noreferrer"
     >
